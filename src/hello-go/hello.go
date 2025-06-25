a51a0f1fdb66055a5c2b98a8e2ecce0b8e5188b8
Patrick Evans <ppevans11@gmail.com>
2025-06-25 14:26:46 +1200

    :alembic: Update `src/hello-go/hello.go`

--- src/hello-go/hello.go
+++ src/hello-go/hello.go
@@ -6,8 +6,17 @@ package main
 
 import "fmt"
 
+// A function can return any number of results.
+// The `swap`` function returns two strings.
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Printf("hello, world\n")
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
 
 /* EOF */
