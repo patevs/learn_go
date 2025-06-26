b8238cf21a15cd8e4fcbb74716a8b4aaf1d3c20b
Patrick Evans <ppevans11@gmail.com>
2025-06-26 15:02:18 +1200

    :alembic: Update `src/hello-go/hello.go`

--- src/hello-go/hello.go
+++ src/hello-go/hello.go
@@ -27,6 +27,31 @@ func split(sum int) (x, y int) {
 	return
 }
 
+// Variables
+//
+// The `var` statement declares a list of variables; as in function argument lists, the type is last.
+// A `var` statement can be at package or function level. We see both in this example.
+var c, python, java bool
+
+// Variables with initializers
+//
+// A var declaration can include initializers, one per variable.
+// If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
+var i, j int = 1, 2
+
+// Short variable declarations
+//
+// Inside a function, the `:=` short assignment statement can be used in place of a `var` declaration with implicit type.
+// Outside a function, every statement begins with a keyword (`var`, `func`, and so on) and so the `:=` construct is not available.
+func shortVariableDeclarations() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
+
+// Program entry point
 func main() {
 	fmt.Printf("hello, world\n")
 
@@ -36,6 +61,17 @@ func main() {
 
 	// Named return values
 	fmt.Println(split(17))
+
+	// Variables
+	var i int
+	fmt.Println(i, c, python, java)
+
+	// Variables with initializers
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+
+	// Short variable declarations
+	shortVariableDeclarations()
 }
 
 /* EOF */
