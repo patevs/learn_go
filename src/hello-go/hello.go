e04ffde5b3d3cdb4486c48aa7349d43fbc12fe1f
Patrick Evans <ppevans11@gmail.com>
2025-06-27 11:41:25 +1200

    :bulb: Comments in `src/hello-go/hello.go`

--- src/hello-go/hello.go
+++ src/hello-go/hello.go
@@ -125,7 +125,7 @@ func zeroValues() {
 // u := uint(f)
 // ```
 // Unlike in C, in Go assignment between items of different type requires an explicit conversion.
-// Try removing the float64 or uint conversions in the example and see what happens.
+// Try removing the `float64` or `uint` conversions in the example and see what happens.
 func typeConversions() {
 	var x, y int = 3, 4
 	var f float64 = math.Sqrt(float64(x*x + y*y))
